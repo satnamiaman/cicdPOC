42937ad40dcd08a9c39b2f6ffc02f51036057b32
Aman <zzbazinga@users.noreply.github.com>
2022-07-13 22:54:45 +0530

    Add files via upload

---
+++ src/main/java/com/aman/cicdPOC/CicdPocApplication.java
@@ -0,0 +1,13 @@
+package com.aman.cicdPOC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aman/cicdPOC/CicdPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aman.cicdPOC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
